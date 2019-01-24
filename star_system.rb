c4df8f433f96ea921af6abe881ce319e75dab117
Alexkcshing <alexkcshing@gmail.com>
2019-01-24 14:57:08 +0000

    pass max test

--- star_system.rb
+++ star_system.rb
@@ -13,7 +13,20 @@ class StarSystem
   end
 
   def planet_names
-    return @planets.map{|planet| planet.name} 
+    return @planets.map{|planet| planet.name}
   end
 
+  def get_planet_by_name(planet_to_find)
+    @planets.find {|planet| planet.name == planet_to_find}
+  end
+
+  def get_largest_planet
+    # largest_planet = @planets.max { |planet| planet.diameter}
+    # return largest_planet.name
+    @planets.reduce(planets[0]) {|big, planet| big.diameter > planet.diameter ? big : planet}
+    # return @planets.diameter.max
+    # diameters = @planets.map {|planet| planet.diameter}
+    # p diameters.max
+    # @planets.max{ |planet| planet.diameter }
+  end
 end
